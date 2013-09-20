a12cf0bfcbd67d87242bb4d1fca37806df5742c4
Koziolek <bjkuczynski@gmail.com>
2013-09-21 01:49:15 +0200

    Kolejny element. Coś czuję, że to będzie dużo kodowania.

---
+++ src/main/java/pl/koziolekweb/progvaadin/componentsexamples/LinkNode.java
@@ -0,0 +1,11 @@
+package pl.koziolekweb.progvaadin.componentsexamples;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: koziolek
+ * Date: 21.09.13
+ * Time: 01:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class LinkNode {
+}

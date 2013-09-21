f804764eabf0e5fc5313d666e78bcf249c823463
Koziolek <bjkuczynski@gmail.com>
2013-09-21 14:57:36 +0200

    Kolejna porcja elementow

---
+++ src/main/java/pl/koziolekweb/progvaadin/componentsexamples/InputNode.java
@@ -0,0 +1,30 @@
+package pl.koziolekweb.progvaadin.componentsexamples;
+
+import com.vaadin.ui.HorizontalSplitPanel;
+import com.vaadin.ui.TextField;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: koziolek
+ * Date: 21.09.13
+ * Time: 01:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class InputNode extends AbstractItemNode {
+	public InputNode(HorizontalSplitPanel target) {
+		super(target);
+	}
+
+	@Override
+	public void onClick() {
+		TextField textField = new TextField();
+		setExample(textField);
+		setSourceCode("TextField textField = new TextField();");
+		done();
+	}
+
+	@Override
+	public String toString() {
+		return "Pole tekstowe";
+	}
+}
---
+++ src/main/java/pl/koziolekweb/progvaadin/componentsexamples/PasswordNode.java
@@ -0,0 +1,11 @@
+package pl.koziolekweb.progvaadin.componentsexamples;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: koziolek
+ * Date: 21.09.13
+ * Time: 14:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class PasswordNode {
+}
